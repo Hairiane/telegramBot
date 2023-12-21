53b221778ea3c87c74ac078dca4cca3449a1dce4
Hairiane <marat.marat.nurgaleev@gmail.com>
2023-12-21 12:36:17 +0500

    add people json

--- BirthdayGPN/index.js
+++ BirthdayGPN/index.js
@@ -1,8 +1,33 @@
 const TelegeramApi = require("node-telegram-bot-api");
-
+const people = require("./people.json");
+const axios = require("axios");
+const proxy = require("https-proxy-agent");
 const token = "6653702940:AAGS3kq4JPMSC621R83itjEW3k52zm12Ghc";
 
+const OpenAI = require("openai");
+
+const openaiapi = new OpenAI.OpenAI({
+  // apiKey: "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",
+  apiKey: "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",
+  httpAgent: new proxy.HttpsProxyAgent("http://173.245.49.3:80"),
+  defaultHeaders: {"Content-Type": "application/json"},
+});
+
 const bot = new TelegeramApi(token, {polling: true});
+
+const citesUrl = ["Ufa", "Moscow", "Kazan'", "Omsk"];
+
+// const urlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=54.7569&lon=56.0129&appid=4057aa5aa77e4f9c621dae0184debe81`;
+// const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=54.7105216&llon=55.835622&appid=be50eb1ea6d54e5d153bd213f89417a9`;
+
+const cites = {
+  Ufa: "Уфе",
+  Moscow: "Москве",
+  "Kazan'": "Казани",
+  Omsk: "Омске",
+};
+
+const history = [];
 
 const img = [
   "https://tlgrm.eu/_/stickers/711/2ce/7112ce51-3cc1-42ca-8de7-62e7525dc332/18.webp",
@@ -18,9 +43,13 @@ const img = [
 ];
 
 const notes = new Map();
-notes.set("5.12", ["@Ilgiz_Sharafutdinov", "@Hairianee"]);
+people.people.map(dr => {
+  if (notes.get(dr.DayMount)) {
+    return notes.set(dr.DayMount, [...notes.get(dr.DayMount), dr.name]);
+  }
+  notes.set(dr.DayMount, [dr.name]);
+});
 
-console.log(notes);
 // const commands = [
 //   {
 //     command: "setbirthday",
@@ -74,24 +103,72 @@ const start = () => {
   setInterval(async () => {
     for (let i = 0; i < notes.size; i++) {
       if (notes.get(`${new Date().getDate()}.${new Date().getMonth() + 1}`)) {
-        if (new Date().getSeconds() === 0) {
-          await bot.sendSticker(
-            -4077749408,
-            img[Math.floor(Math.random() * 10)],
-          );
-          return bot.sendMessage(
-            "-4077749408",
-            `С днём рождения ${notes
-              .get(`${new Date().getDate()}.${new Date().getMonth() + 1}`)
-              .map(arr => arr)}`,
-            //   .map(arr => arr)}`,
-          );
-        }
+      if (new Date().getSeconds() === 0 && notes.get(`24.3`)) {
+        await bot.sendSticker(-4077749408, img[Math.floor(Math.random() * 10)]);
+        return bot.sendMessage(
+          "-4077749408",
+          `С днём рождения ${notes
+            .get(`${new Date().getDate()}.${new Date().getMonth() + 1}`)
+            .map(arr => arr)}`,
+          //   .map(arr => arr)}`,
+        );
+          }
       }
     }
     if (new Date().getDay() == 4 && new Date().getHours() == 15) {
       return bot.sendMessage("-4077749408", "На созвон просыпаемся!!");
     }
+    if (
+      (new Date().getDay() == 4 && new Date().getHours() == 15) ||
+      new Date().getSeconds() === 0
+    ) {
+      // citesUrl.map(async city => {
+      //   const response = await axios.get(
+      //     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=be50eb1ea6d54e5d153bd213f89417a9`,
+      //   );
+      //   console.log(response.data);
+      //   return bot.sendMessage(
+      //     "-4077749408",
+      //     `
+      //   Сегоднящняя погода в ${cites[response.data.name]} ${(
+      //       response.data.main.temp - 273.15
+      //     ).toFixed(1)}
+      //     Ощущается как ${(response.data.main.feels_like - 273.15).toFixed(1)}
+      //     Минимальная температура сегодня ${(
+      //       response.data.main.temp_min - 273.15
+      //     ).toFixed(1)}
+      //     Максимальная температура сегодня ${(
+      //       response.data.main.temp_max - 273.15
+      //     ).toFixed(1)}
+      //     Видимость ${response.data.visibility}
+      //   `,
+      //   );
+      // });
+    }
+    if (
+      (new Date().getDay() == 4 && new Date().getHours() == 15) ||
+      new Date().getSeconds() == 20 ||
+      new Date().getSeconds() == 0 ||
+      new Date().getSeconds() == 30
+    ) {
+      // history.push({})
+      // const res = await openaiapi.chat.completions.create({
+      //   messages: [
+      //     {
+      //       role: "user",
+      //       content: "Hello",
+      //       // "Напиши простенькую задачу для frontend javascript разработчика",
+      //     },
+      //   ],
+      //   model: "gpt-3.5-turbo",
+      //   response_format: {type: "json_object"},
+      // });
+      // return bot.sendMessage(
+      //   "-4077749408",
+      //   res.data.choices[0].message.content,
+      // );
+    }
+    return null;
   }, 1000);
 };
 
